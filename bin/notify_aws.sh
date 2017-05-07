63071f34fdd2fefb51d26ec81faaf2631f88d3bc
Samuel Terburg <Samuel.Terburg@Panther-IT.nl>
2017-05-07 11:04:30 +0000

    Improved re-association count

--- bin/notify_aws.sh
+++ bin/notify_aws.sh
@@ -6,9 +6,12 @@ AWS_EC2_INSTANCE_ID=$(ec2metadata --instance-id)
 VRRP_STATE=${3:-BACKUP}
 
 for VIRTUAL_IP in $(expand_ip_ranges "${OPENSHIFT_HA_VIRTUAL_IPS}"); do
-  aws ec2 disassociate-address --public-ip $VIRTUAL_IP
   if [ "$VRRP_STATE" == "MASTER" ]; then
-     aws ec2 associate-address    --public-ip $VIRTUAL_IP --instance-id $AWS_EC2_INSTANCE_ID
+    AWS_EC2_CURRENT_INSTANCE_ID=`aws ec2 describe-addresses --public-ips=$VIRTUAL_IP --query='Addresses[0].InstanceId' --output text`
+    if [ "${AWS_EC2_CURRENT_INSTANCE_ID}" != "${AWS_EC2_INSTANCE_ID}"]; then
+      aws ec2 disassociate-address --public-ip $VIRTUAL_IP
+      aws ec2 associate-address    --public-ip $VIRTUAL_IP --instance-id $AWS_EC2_INSTANCE_ID
+    fi
   fi
 done
 
