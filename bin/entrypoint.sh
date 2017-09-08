c55d2e1e58f29d58ed824526ae104e2a0403a489
Samuel Terburg <github.com@panther-it.nl>
2017-09-08 14:46:27 +0200

    Update entrypoint.sh

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,7 +2,7 @@
 
 OPENSHIFT_HA_NOTIFY_SCRIPT=${OPENSHIFT_HA_NOTIFY_SCRIPT:-/usr/local/bin/notify_aws.sh}
 
-envsubst < /root/.aws/config.tpl       >/root/.aws/config
-envsubst < /root/.aws/credentials.tpl  >/root/.aws/credentials
+[ ! -f /root/.aws/config      ] && envsubst < /root/.aws/config.tpl       >/root/.aws/config
+[ ! -f /root/.aws/credentials ] && envsubst < /root/.aws/credentials.tpl  >/root/.aws/credentials
 
 exec /var/lib/ipfailover/keepalived/monitor.sh $@
